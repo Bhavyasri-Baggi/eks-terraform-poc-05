a3ac8477e7def16dc63146b48b2c0c09428bf9ce
Bhavyasri-Baggi <bhavyabaggi407@gmail.com>
2026-04-22 15:11:09 +0000

    fixed subnet + vpc config

--- main.tf
+++ main.tf
@@ -1,6 +1,3 @@
-
-  subnet_ids = ["subnet-07590f506ec22c074", "subnet-0315b09c5f5c529b0","subnet-09888a606d4a7fe2e"]
-
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
@@ -8,7 +5,13 @@ module "eks" {
   cluster_name    = "demo-eks"
   cluster_version = "1.29"
 
-  subnet_ids = ["subnet-07590f506ec22c074", "subnet-0315b09c5f5c529b0","subnet-09888a606d4a7fe2e"]
+  vpc_id = "vpc-02a998371885782dc"
+
+  subnet_ids = [
+    "subnet-07590f506ec22c074",
+    "subnet-0315b09c5f5c529b0",
+    "subnet-09888a606d4a7fe2e"
+  ]
 
   eks_managed_node_groups = {
     default = {
@@ -18,4 +21,4 @@ module "eks" {
       max_size       = 2
     }
   }
-} 
+}
