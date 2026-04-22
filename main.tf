ab97c8a540f40ca14f4618cc1dc9ee2d0b2c5b93
Bhavyasri-Baggi <bhavyabaggi407@gmail.com>
2026-04-22 15:04:10 +0000

    fixed eks module version

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
+
+  subnet_ids = ["subnet-07590f506ec22c074", "subnet-0315b09c5f5c529b0","subnet-09888a606d4a7fe2e"]
+
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
   cluster_name    = "demo-eks"
   cluster_version = "1.29"
 
@@ -8,7 +13,9 @@ module "eks" {
   eks_managed_node_groups = {
     default = {
       instance_types = ["t3.medium"]
-      desired_size = 1
+      desired_size   = 1
+      min_size       = 1
+      max_size       = 2
     }
   }
-}
+} 
